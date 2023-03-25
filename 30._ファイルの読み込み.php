bb2934be87e6b86aca32b549d96b12301c8a76fb
devYuMinKim <gimyumin40@gmail.com>
2023-03-25 15:08:28 +0900

    Feat : ファイルの読み込み

---
+++ 30._ファイルの読み込み.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '30._ファイルの読み込み_test.php';
+
+echo $commonVariable; // 共通の変数です
+
+commonTest(); // 外部ファイルの関数です
+
+echo __DIR__; // /Applications/MAMP/htdocs/php_test
+
+?>
---
+++ 30._ファイルの読み込み_test.php
@@ -0,0 +1,9 @@
+<?php
+
+$commonVariable = "共通の変数です";
+
+function commonTest() {
+  echo '外部ファイルの関数です';
+}
+
+?>
